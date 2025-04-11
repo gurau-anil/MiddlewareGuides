c7a4c70549af93b515b63fd46ee2d5994b579591
gurau-anil <gurau.anil@gmail.com>
2025-04-10 21:48:32 -0700

    Timing middleware

---
+++ MiddlewareGuides/Middlewares/TimingMiddleware.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+
+namespace MiddlewareGuides.Middlewares
+{
+    public class TimingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public TimingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            var stopwatch = Stopwatch.StartNew();
+            await _next(context);
+            stopwatch.Stop();
+
+            Console.WriteLine($"Request took {stopwatch.ElapsedMilliseconds} ms");
+        }
+    }
+
+}
